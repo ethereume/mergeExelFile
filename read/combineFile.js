b3925284bbcf002e49b4fbc24ffe8de0b9d22e0e
DeXter1986Golkow <mzawadzki.developer@gmail.com>
2018-06-14 21:24:29 +0200

    Algorytm pierwszy

--- read/combineFile.js
+++ read/combineFile.js
@@ -48,8 +48,40 @@ const getDataFromSheet = (number) =>{
 	return tmpTab;
 }
 const makeFile = (data) => {
-	
+		let tab = [];
+		data.forEach((obj)=>{
+				tab.push({v:obj.sheet});
+				obj.parameter.forEach((pojedyncza)=>{
+					//console.log(pojedyncza);
+					tab = [...tab,...pojedyncza];
+				});
+		});
+		
 	};
+
+function sheet_from_array_of_arrays(data) {
+	var ws = {};
+	var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+	for(var R = 0; R != data.length; ++R) {
+		for(var C = 0; C != data[R].length; ++C) {
+			if(range.s.r > R) range.s.r = R;
+			if(range.s.c > C) range.s.c = C;
+			if(range.e.r < R) range.e.r = R;
+			if(range.e.c < C) range.e.c = C;
+			var cell = {v: data[R][C] };
+			if(cell.v == null) continue;
+			var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+			
+			if(typeof cell.v === 'number') cell.t = 'n';
+			else if(typeof cell.v === 'boolean') cell.t = 'b';
+			else cell.t = 's';
+			
+			ws[cell_ref] = cell;
+		}
+	}
+	if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+	return ws;
+}
 const makeEXEL = (fileName) => {
 
 	function Workbook() {
@@ -59,8 +91,7 @@ const makeEXEL = (fileName) => {
 	}
 	let work = new Workbook();
 	work.SheetNames.push(fileName);
-	console.log(tabsArray);
-	//work[fileName] = makeFile(tabsArray);
+	work[fileName] = makeFile(tabsArray);
 	//console.log(work[fileName]);
 	return work;
 }
